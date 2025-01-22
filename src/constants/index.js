b10b4834cf6362ce0fbf49102af7313f51bb9405
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2025-01-22 20:42:28 +0100

    portofolio_website

---
+++ src/constants/index.js
@@ -0,0 +1,281 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Emily Johnson',
+    position: 'Marketing Director at GreenLeaf',
+    img: 'assets/review1.png',
+    review:
+      'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Mark Rogers',
+    position: 'Founder of TechGear Shop',
+    img: 'assets/review2.png',
+    review:
+      'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'John Dohsas',
+    position: 'Project Manager at UrbanTech ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Ether Smith',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:
+      'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Transactly -  Online Banking Platform',
+    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    subdesc:
+      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+    href: 'https://transactlyy.vercel.app/sign-in',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/login.svg',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: ' Next js',
+        path: '/assets/nextjs.png',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Appwrite',
+        path: '/assets/appwrite.png',
+      },
+     ,
+    ],
+  },
+ 
+  {
+    title: 'Weeshr - Weeshr official website',
+    desc: 'Weeshr makes it easy for you to get the gift you want and deserve for your celebrations and occasions.',
+    subdesc:
+      'Built with Next.js 14,typescript,tailwind css and shadcn it ensures that you collect Weeshr helps you collect your gifts with the click of one button from your friends, fans, and family!',
+    href: 'https://www.weeshr.com',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/weeshr_logo.svg',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: ' Next js',
+        path: '/assets/nextjs.png',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Shadcn',
+        path: '/assets/shadcn.png',
+      },
+    ],
+  },
+  {
+    title: 'Weeshr Admin App - Weeshr Administration Application',
+    desc: 'The Weeshr admin website is a robust and efficient platform designed to streamline administrative tasks, enhance data management, and provide insightful analytics for Weeshr Operations. The admin panel prioritizes user experience, offering a seamless and intuitive interface to handle complex workflows and manage key functionalities effectively.',
+    subdesc:
+      'The website is built using Vue.js for its reactive and component-driven architecture. It incorporates ShadCN UI for cohesive and modern design, Tailwind CSS for rapid and responsive styling, and TypeScript to ensure type safety and maintainable code. This stack ensures the admin panel is both powerful and scalable.',
+    href: 'https://www.weeshr.com/',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/weeshr_logo.svg',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Vuejs',
+        path: '/assets/vue.png',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Shadcn',
+        path: '/assets/shadcn.png',
+      },
+    ],
+  },
+  {
+    title: 'Portfolio Website - Olawale Awokoya',
+    desc: 'The portfolio website showcases a dynamic and visually engaging platform that highlights personal achievements, projects, and skills. Designed to leave a lasting impression, it blends interactivity and creativity to present a professional yet unique online presence.',
+    subdesc:
+      'The website is developed with React.js for its efficient component-based structure and Three.js to incorporate immersive 3D elements. JavaScript powers the sites interactivity, ensuring a smooth and engaging user experience.',
+    href: 'https://www.olawale.vercel.app',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/logo.jpg',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+        path: 'assets/JavaScript.png',
+      },
+      {
+        id: 3,
+        name: 'Vitejs',
+        path: '/assets/Vitejs.png',
+      },
+      {
+        id: 4,
+        name: 'Threejs',
+        path: '/assets/three-js.png',
+      },
+    ],
+  },
+  {
+    title: 'Davidson - First Portfolio Website',
+    desc: 'The portfolio website is a clean and professional platform designed to showcase skills, accomplishments, and projects. It provides visitors with an intuitive and aesthetically pleasing experience, focusing on simplicity and effective communication.',
+    subdesc:
+      'The website is built using HTML for structured content, CSS for styling and layout, and JavaScript for interactive functionality. This combination ensures a visually appealing and responsive design while maintaining high performance.',
+    href: 'https://www.davidson.vercel.app',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/logo.jpg',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Html',
+        path: '/assets/html.png',
+      },
+      {
+        id: 2,
+        name: 'JavaScript',
+        path: 'assets/JavaScript.png',
+      },
+      
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Weeshr',
+    pos: 'Frontend Developer',
+    duration: '2023 - Present',
+    title: "Utilize Vue.js and Next.js 14 to build scalable and performant applications and Employed TypeScript for type safety and to ensure robust code quality, resulting in fewer runtime errors and improved maintainability",
+    icon: '/assets/weeshr_logo.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Upwork',
+    pos: 'Web Developer',
+    duration: '2022 - present',
+    title: "Utilize Threejs,vuejs,nextjs and react to build scalable and performant applications and Employed TypeScript for type safety and to ensure robust code quality, resulting in fewer runtime errors and improved maintainability",
+    icon: '/assets/upwork.png',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Fuoye',
+    pos: 'Self Development',
+    duration: '2022 - 2023',
+    title: "•	Contributed to the development of contemporary and user-friendly interfaces by employing HTML, CSS, and JavaScript and adapted to evolving project specifications and technological advancements to stay informed and align projects with specifications and deadlines.",
+    icon: '/assets/fuoye.jpeg',
+    animation: 'salute',
+  },
+];
